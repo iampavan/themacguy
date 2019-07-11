e7efde224c33bdcfd9eeb3e94ada606da989daea
Pavan Raju <praju@expediagroup.com>
2019-07-12 00:02:31 +0530

    Added first post

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "My First Blog Post",
+        "excerpt":"print ‘Hello, World!’  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/01-hello-world/",
+        "teaser":null}]
